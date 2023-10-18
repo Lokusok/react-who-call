5ba709e04776b02b344453295381f473c5bc9304
Lokusok <lokusokdrok@gmail.com>
2023-10-18 18:46:40 +0300

    remove all

--- src/hooks/useScrollTop.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const useScrollTop = () => {
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  });
-};
-
-export default useScrollTop;
--- src/pages/Register/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import RegisterForm from './RegisterForm';
-
-const Register: React.FC = () => {
-  return <RegisterForm />;
-};
-
-export default Register;
